1dc44c0c650319c07671d1a39844213cb1f03cdf
Uwais Jawed <uwaisjawed8081@gmail.com>
2023-07-23 15:42:15 +0530

    Test

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">Heera Holiday Hotel</span>
+      </h1>
+      <p className="desc text-center">
+        Welcome to Heera Holiday! You can easily book luxury,deluxe rooms at
+        economical cost. Food and room managing utilities are also provided
+      </p>
+    </section>
+  );
+};
+
+export default Home;
